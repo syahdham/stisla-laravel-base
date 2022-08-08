a789a86e28eb178f62a12e5ac6afade0e26e3e29
syahdham <syahdham@gmail.com>
2022-08-08 20:00:50 +0700

    Menambahkan Constants untuk return message

---
+++ app/Constants/MessageConstant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class MessageConstant
+{
+    const FORBIDDEN_403 = '403 Forbidden';
+    const SAVED_SUCCESSFULLY = 'Data berhasil disimpan';
+    const UPDATED_SUCCESSFULLY = 'Data berhasil disimpan';
+    const DELETED_SUCCESSFULLY = 'Data berhasil dihapus';
+    const SERVER_ERROR = 'Terdapat eror server';
+}
